df4ee0455ecb69072fdbc17c48c14ea9f550294a
Gleisson Braga <bragagleisson@gmail.com>
2024-07-02 07:21:09 -0300

    feat: adicionando session no projeto

---
+++ admin/controller/connect.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$dbName = "db_php"; // colocar nome do db
+$connect = mysqli_connect($server, $user, $pass, $dbName);
+
+function login($connect) {
+    if(isset($_POST['logar'])) {
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $email = mysqli_real_escape_string($connect, $email);
+        $senha = sha1($_POST['senha']);
+
+        if(!empty($email) and !empty($senha)) {
+            $query = "SELECT *FROM users WHERE email = '$email' AND senha = '$senha' ";
+
+            $action = mysqli_query($connect, $query);
+            $result = mysqli_fetch_assoc($action);
+            if(!empty($result)) {
+                session_start();
+                $_SESSION['nome'] = $result['nome'];
+                $_SESSION['active'] = true;
+                header("location: area_adm.php");
+            } else {
+                $_SESSION['error_message'] = "Email ou senha incorretos";
+            }
+        }
+    }
+}
---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<?php require_once("controller/connect.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Área do administrador</title>
+</head>
+<body>
+    <?php login($connect); ?>
+    <main id="container">
+        <section class="conteudo-login">
+            <h1>Bem vindo!</h1>
+            <h2>Área do Administrador</h2>
+            
+            <form action="" method="post" class="form">
+                <input type="email" name="email" id="email" placeholder="Email" require autofocus class="text">
+                <input type="password" name="senha" id="senha" placeholder="Senha" require class="text">
+                <?php
+                    if (isset($_SESSION['error_message'])) {
+                        echo "<p class='msg-erro'>" . $_SESSION['error_message'] . "</p>";
+                        unset($_SESSION['error_message']);
+                    }
+                ?>
+                <!-- <p class="msg-erro">Email ou senha incorretos</p> -->
+                <input type="submit" value="Logar" name="logar" class="btn bn632-hover bn26">
+            </form>
+            <a href="http://localhost:8080/texasBurguer/"><button class="bn632-hover bn25"><ion-icon name="arrow-back-circle-sharp"></ion-icon>Ir para o site</button></a>
+        </section>
+        <section class="conteudo-fundo">
+           
+        </section>
+    </main>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
